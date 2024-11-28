2bdba421e54d4b3bcd494b1d5e5db890bcf0959c
Jéssie <126810934+JessieNeire@users.noreply.github.com>
2024-11-28 13:41:31 -0300

    ocultar dados de conexaoBD.php
    ocultei os dados de segurança

--- conexaoBD.php
+++ conexaoBD.php
@@ -1,26 +1,14 @@
 <?php
-// BD para testes PC Leandro
-
-$servername = "localhost";
-$username = "root";
-$password = "Mestrelm1@";
-$dbname = "conpac";
-$conexao = new mysqli($servername, $username, $password, $dbname);
-if ($conexao->connect_error) {
-die("Connection failed: " . $conexao->connect_error);
-}
 
 
-//BD temporario teste Jéssie
-/*  
 $servername = "localhost:3307";
-$username = "root";
-$password = "usbw"; 
+$username = "******";
+$password = "*****"; 
 $dbname = "conpac";
 $conexao = new mysqli($servername, $username, $password, $dbname);
 if ($conexao->connect_error) {
     die("Connection failed: " . $conexao->connect_error);
 } 
-    */ 
 
-    ?>
+
+?>
